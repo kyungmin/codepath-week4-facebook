9002c00013181e954ee30da40b89ed6e6c6cc40c
Kyungmin Kim <kmin905@gmail.com>
2015-02-24 11:08:50 -0800

    Initial Commit

---
+++ Facebook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facebook
+//
+//  Created by Kyungmin Kim on 2/24/15.
+//  Copyright (c) 2015 Kyungmin Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
